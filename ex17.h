e9b4419f77b968866361a460f68551d9d311ff98
Arthur Vinicius Delmas Borges <avdb96@gmail.com>
2018-12-07 20:13:38 -0300

    esqueleto ex17.h

---
+++ ex17.h
@@ -0,0 +1,83 @@
+/***************************************************************************
+ *   exN.h                                 Version 20160609.110111       *
+ *                                                                         *
+ *   Library for exN, a program that converts ER into AFND               *
+ *   Copyright (C) 2016         by Ruben Carlo Benante                     *
+ ***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 2 of the License                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *   To contact the author, please write to:                               *
+ *   Ruben Carlo Benante                                                   *
+ *   Email: rcb@beco.cc                                                    *
+ *   Webpage: http://www.beco.cc                                           *
+ *   Phone: +55 (81) 3184-7555                                             *
+ ***************************************************************************/
+
+/* ---------------------------------------------------------------------- */
+/* definitions */
+
+#ifndef VERSION
+#define VERSION "20160609.110111" /**< Version Number */
+#endif
+
+/* Debug */
+#ifndef DEBUG /* gcc -DDEBUG=1 */
+#define DEBUG 0 /**< Activate/deactivate debug mode */
+#endif
+
+/** @brief Debug message if DEBUG on */
+#define IFDEBUG(M) if(DEBUG) fprintf(stderr, "[DEBUG file:%s line:%d]: " M "\n", __FILE__, __LINE__); else {;}
+
+/* limits */
+#define SBUFF 256 /**< String buffer */
+
+/* ---------------------------------------------------------------------- */
+/* globals */
+
+static int verb=0; /**< verbose level, global within the file */
+
+/* tipo lista encadeada simples dos estados finais */
+typedef struct st_lef
+{
+    unsigned short int f;
+    struct st_lef *prox;
+} t_lef;
+
+/* tipo lista encadeada simples da funcao de transicao */
+typedef struct st_lft
+{
+    unsigned short int ei;  /* estado inicial */
+    char *le;               /* le (string com rotulos) */
+    unsigned short int ef;  /* estado final */
+    struct st_lft *prox;
+} t_lft;
+
+/* tipo quintupla: representacao formal de um automato */
+typedef struct st_quintupla
+{
+    unsigned short int K;   /* conjunto de estados */
+    char A;                 /* alfabeto */
+    unsigned short int S;   /* estado inicial */
+    t_lef *F;               /* lista de estados finais */
+    t_lft *D;               /* lista da funcao de transicao d(ei, le, ef) */
+} t_quintupla;
+
+/* ---------------------------------------------------------------------- */
+/* prototypes */
+
+void help(void); /* print some help */
+void copyr(void); /* print version and copyright information */
+void exN_init(void); /* global initialization function */
+
