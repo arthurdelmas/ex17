d297cf94cf5f662a6fd804e7f8dbc9124de70991
Bruno Antonio Pereira Cavalcanti <cavalcantibruno@ymail.com>
2018-12-12 12:26:48 -0300

    adicao do prototipo transformacao

--- ex17.h
+++ ex17.h
@@ -83,5 +83,5 @@ void exN_init(void); /* global initialization function */
 int tipo_operador(char info)
 void quebraExpressao(char *expReg, t_arvore **raiz); /*quebra a expressao e armazena na arvore binaria*/    
 void separador(char *expReg, char **partEsq, char **partDir, char *op, int size); /*separa a expressao regular no ponto pre determinado*/
-
+void transformacao(t_arvore *raiz); /* realiza as operacoes de acordo com a arvore binaria, na ordem pos ordem */
 
